2a3ce13aa104688f86cb4796f2245fc02c11254a
kevincarrillo032 <132705753+kevincarrillo032@users.noreply.github.com>
2024-05-29 12:13:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import pickle
+import os
 
 # Load the saved model
 model = pickle.load(open('churnmodel.pkl', 'rb'))
@@ -63,4 +64,5 @@ def predict_churn():
     return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
